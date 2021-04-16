3e3d649d6323d8a3d8200533af3886d5562132c1
Rohan <rhnshiva@gmail.com>
2021-04-16 10:36:28 -0500

    Delete user.py

--- user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, key, username, email, avatar_url):
-        self.key = key
-        self.username = username, 
-        self.email = email,
-        self.avatar_url = avatar_url
-    
